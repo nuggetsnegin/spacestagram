b77c3d19826644fa723aad84d6deee9a3e3df267
Negin <nuggetnegin@gmail.com>
2022-01-19 14:29:43 -0500

    Refactor API request - add scaffolding for loading state - move function into useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -6,38 +6,47 @@ import Card from './Card';
 
 function App() {
   const [data, setData] = useState([]);
-
-  async function fetchNasaPhotos() {
-    const response = await fetch(
-      `https://api.nasa.gov/planetary/apod?count=30&api_key=${API_KEY}`
-    );
-    if (!response.ok) {
-      throw new Error(
-        `Woops, something wen't wrong! status: ${response.status}`
-      );
-    }
-    setData(await response.json());
-  }
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    const fetchNasaPhotos = async () => {
+      setIsLoading(true);
+      const response = await fetch(
+        `https://api.nasa.gov/planetary/apod?count=30&api_key=${API_KEY}`
+      );
+      if (!response.ok) {
+        throw new Error(
+          `Woops, something wen't wrong! status: ${response.status}`
+        );
+      }
+      const responseJson = await response.json();
+      setIsLoading(false);
+      setData(responseJson);
+    };
     fetchNasaPhotos();
-  }, []);
+  }, [setData]);
 
   return (
     <div className='App'>
       <header className='App-header'>
         <h1>Spacetagram</h1>
       </header>
-      {data.map((item: Post) => (
-        <Card
-          title={item.title}
-          url={item.url}
-          explanation={item.explanation}
-          date={item.date}
-          copyright={item.copyright}
-          key={JSON.stringify(item)}
-        />
-      ))}
+      <main>
+        {isLoading ? (
+          <h2>peepo</h2>
+        ) : (
+          data.map((item: Post) => (
+            <Card
+              title={item.title}
+              url={item.url}
+              explanation={item.explanation}
+              date={item.date}
+              copyright={item.copyright}
+              key={JSON.stringify(item)}
+            />
+          ))
+        )}
+      </main>
     </div>
   );
 }
